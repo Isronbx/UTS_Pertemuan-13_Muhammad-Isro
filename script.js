15fe50ea6c66089890a0a5658d4a591adb34a3bb
Isronbx <misyharyisro123@gmail.com>
2024-10-21 13:53:40 +0700

    Landing Page Darusssalam

---
+++ script.js
@@ -0,0 +1,66 @@
+/* ======================== toggle icon navbar ==================*/
+let menuIcon = document.querySelector ('#menu-icon');
+let navbar = document.querySelector ('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+
+};
+
+
+/* ======================== scroll section avitive link ==================*/
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id +']' ).classList.add('active');
+            });
+
+        };
+
+    });
+
+
+/* ======================== sticky navbar ==================*/
+
+let header = document.querySelector('header');
+
+header.classList.toggle('sticky', window.scrollY > 100);
+
+/* ======================== remove toggle icon navbar when click navbar link (scroll) ==================*/
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+};
+
+/* ======================== scroll reveal ==================*/
+ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portofolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+
+/* ======================== typed Js ==================*/
+const typed = new Typed ('.multiple-text', {
+    strings: ['Hafizh Al-Quran', 'Alim Ulama', 'Dai', 'Public Figure', 'Moslem Entrepreneurship'],
+    typeSpeed: 100,
+    typeSpeed: 100,
+    backDelay: 1000,
+    loop:true
+
+});
